d96271f2eeb23bb83b58fb53b4422c9a2ac0d9c7
Bastian <b.waidelich@wwwision.de>
2017-08-07 14:37:39 +0200

    FEATURE: Localization

--- Classes/Eel/Helper/FormHelper.php
+++ Classes/Eel/Helper/FormHelper.php
@@ -3,13 +3,23 @@ namespace Neos\Form\FusionRenderer\Eel\Helper;
 
 use Neos\Eel\ProtectedContextAwareInterface;
 use Neos\Error\Messages\Result;
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\I18n\Translator;
 use Neos\Flow\Mvc\ActionRequest;
+use Neos\Flow\ResourceManagement\Exception as ResourceException;
 use Neos\Form\Core\Model\FormElementInterface;
+use Neos\Form\Core\Model\Renderable\AbstractRenderable;
 use Neos\Form\Core\Runtime\FormRuntime;
 use Neos\Utility\ObjectAccess;
 
 class FormHelper implements ProtectedContextAwareInterface
 {
+
+    /**
+     * @Flow\Inject
+     * @var Translator
+     */
+    protected $translator;
 
     public function elementValue(FormRuntime $formRuntime, FormElementInterface $element)
     {
@@ -49,6 +59,43 @@ class FormHelper implements ProtectedContextAwareInterface
             return new Result();
         }
         return $validationResults->forProperty($element->getIdentifier());
+    }
+
+    public function translateAndEscapeProperty(AbstractRenderable $element, string $property): string
+    {
+        return $this->escape($this->translateProperty($element, $property));
+    }
+
+    public function translateProperty(AbstractRenderable $element, string $property): string
+    {
+        if ($property === 'label') {
+            $defaultValue = $element->getLabel();
+        } elseif ($element instanceof FormElementInterface) {
+            $defaultValue = isset($element->getProperties()[$property]) ? (string)$element->getProperties()[$property] : '';
+        } else {
+            $defaultValue = '';
+        }
+        $translationId = sprintf('forms.elements.%s.%s', $element->getIdentifier(), $property);
+        return $this->translate($element, $translationId, $defaultValue);
+    }
+
+    public function translate(AbstractRenderable $element, string $translationId, string $defaultValue): string
+    {
+        $renderingOptions = $element->getRenderingOptions();
+        if (!isset($renderingOptions['translationPackage'])) {
+            return $defaultValue;
+        }
+        try {
+            $translation = $this->translator->translateById($translationId, [], null, null, 'Main', $renderingOptions['translationPackage']);
+        } catch (ResourceException $exception) {
+            return $defaultValue;
+        }
+        return $translation ?? $defaultValue;
+    }
+
+    public function escape(string $string): string
+    {
+        return htmlspecialchars($string, ENT_QUOTES);
     }
 
     /**
---
+++ Classes/Fusion/SelectOptionsImplementation.php
@@ -0,0 +1,73 @@
+<?php
+namespace Neos\Form\FusionRenderer\Fusion;
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\I18n\Translator;
+use Neos\Flow\ResourceManagement\Exception as ResourceException;
+use Neos\Form\Core\Model\FormElementInterface;
+use Neos\Fusion\Exception as FusionException;
+use Neos\Fusion\FusionObjects\AbstractFusionObject;
+
+class SelectOptionsImplementation extends AbstractFusionObject
+{
+
+    /**
+     * @Flow\Inject
+     * @var Translator
+     */
+    protected $translator;
+
+    public function evaluate()
+    {
+        $context = $this->runtime->getCurrentContext();
+        if (!isset($context['element'])) {
+            throw new FusionException('Missing "element" in the Fusion context', 1502108256);
+        }
+        $element = $context['element'];
+        if (!$element instanceof FormElementInterface) {
+            throw new FusionException(sprintf('SelectOptions object can only be used within FormElementInterface elements, given: %s', is_object($element) ? get_class($element) : gettype($element)), 1502108340);
+        }
+        if (!isset($element->getProperties()['options'])) {
+            throw new FusionException('The current element doesn\'t have an "options" property', 1502108512);
+        }
+        $output = '';
+        foreach ($element->getProperties()['options'] as $optionValue => $originalLabel) {
+            $translationId = sprintf('forms.elements.%s.options.%s', $element->getIdentifier(), $optionValue);
+            $optionLabel = htmlspecialchars($this->translate($element, $translationId, $originalLabel), ENT_QUOTES);
+            $context['optionValue'] = $optionValue;
+            $context['optionLabel'] = $optionLabel;
+            $context['optionSelected'] = $this->isOptionSelected($optionValue);
+
+            $this->runtime->pushContextArray($context);
+            $output .= $this->runtime->render($this->path . '/itemRenderer');
+            $this->runtime->popContext();
+        }
+        return $output;
+    }
+
+    public function translate(FormElementInterface $element, string $translationId, string $defaultValue): string
+    {
+        $renderingOptions = $element->getRenderingOptions();
+        if (!isset($renderingOptions['translationPackage'])) {
+            return $defaultValue;
+        }
+        try {
+            $translation = $this->translator->translateById($translationId, [], null, null, 'Main', $renderingOptions['translationPackage']);
+        } catch (ResourceException $exception) {
+            return $defaultValue;
+        }
+        return $translation ?? $defaultValue;
+    }
+
+    private function isOptionSelected($optionValue): bool
+    {
+        $elementValue = ($this->runtime->getCurrentContext())['elementValue'] ?? null;
+        if ($optionValue === $elementValue) {
+            return true;
+        }
+        if (is_array($elementValue) && in_array($optionValue, $elementValue)) {
+            return true;
+        }
+        return false;
+    }
+}
